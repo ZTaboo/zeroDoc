bc58c19e2131b1f017b9e6a983a400d631e58cfd
zero <166997982@qq.com>
2022-11-24 00:10:15 +0800

    init

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -10,7 +10,7 @@ const config = {
   tagline: 'gopsutil的文档',
   url: 'https://gopsutil-doc.vercel.app/',
   baseUrl: '/',
-  onBrokenLinks: 'throw',
+  onBrokenLinks: 'ignore',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
 
