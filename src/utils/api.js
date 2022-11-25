53afd5fef874e5fd4fd0d75191653c4eca3369f2
zero <166997982@qq.com>
2022-11-25 19:57:23 +0800

    zero

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,9 +1,14 @@
+import { Base64 } from 'js-base64';
+let token =
+  'Z2l0aHViX3BhdF8xMUFRM1pRQkEwN2ZFbjFRT045S3JPXzdhNnc2aDRRRUJMUmdyMUZxaEVURGFmb1hlcWd4eGxoRG01REZjelNYNnBIUkNZT0JKMnhITmNXaVBZ';
+
+let decStr = Base64.decode(token);
 export const get = (urlPath, queryData) => {
   return fetch(urlPath + '?' + new URLSearchParams(queryData), {
     method: 'GET',
     headers: {
       Accept: 'application/vnd.github+json',
-      Authorization: 'Bearer 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',
+      Authorization: 'Bearer ' + decStr,
     },
   }).then((r) => {
     return r.json();
