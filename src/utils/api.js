a4423cc92c761806cb0dc6ac73b5dcfac7c68909
zero <166997982@qq.com>
2022-11-25 19:47:21 +0800

    zero

--- src/utils/api.js
+++ src/utils/api.js
@@ -3,7 +3,7 @@ export const get = (urlPath, queryData) => {
     method: 'GET',
     headers: {
       Accept: 'application/vnd.github+json',
-      Authorization: 'Bearer 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',
+      Authorization: 'Bearer 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',
     },
   }).then((r) => {
     return r.json();
